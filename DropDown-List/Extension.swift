f7216f17ebbaa28d91e0873638fd394e7ec6af7d
Mohd Tauheed Uddin Siddiqui <dev@reyku.com>
2017-11-16 23:49:56 +0530

    Initial Completed DropDown list

---
+++ DropDown-List/Extension.swift
@@ -0,0 +1,101 @@
+//
+//  Extension.swift
+//  DropDown-List
+//
+//  Created by Mohd Tauheed Uddin Siddiqui on 16/11/17.
+//  Copyright © 2017 Mohd Tauheed Uddin Siddiqui. All rights reserved.
+
+// Inspired from Brian Voong from Lets Build That App [I suggest everyone to subscribe to his channel to be expert in iOS]
+
+import UIKit
+
+extension UIView{
+    func addConstraintsWithFormat(format: String,views  : UIView...){
+        var viewsDictionary = [String: UIView]()
+        for (index,view) in views.enumerated(){
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIView.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        endEditing(true)
+    }
+    
+    
+    func anchorToTop(_ top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil) {
+        
+        anchorWithConstantsToTop(top, left: left, bottom: bottom, right: right, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0)
+    }
+    
+    func anchorWithConstantsToTop(_ top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0) {
+        
+        _ = anchor(top, left: left, bottom: bottom, right: right, topConstant: topConstant, leftConstant: leftConstant, bottomConstant: bottomConstant, rightConstant: rightConstant)
+    }
+    
+    func anchor(_ top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0, widthConstant: CGFloat = 0, heightConstant: CGFloat = 0) -> [NSLayoutConstraint] {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        var anchors = [NSLayoutConstraint]()
+        
+        if let top = top {
+            anchors.append(topAnchor.constraint(equalTo: top, constant: topConstant))
+        }
+        
+        if let left = left {
+            anchors.append(leftAnchor.constraint(equalTo: left, constant: leftConstant))
+        }
+        
+        if let bottom = bottom {
+            anchors.append(bottomAnchor.constraint(equalTo: bottom, constant: -bottomConstant))
+        }
+        
+        if let right = right {
+            anchors.append(rightAnchor.constraint(equalTo: right, constant: -rightConstant))
+        }
+        
+        if widthConstant > 0 {
+            anchors.append(widthAnchor.constraint(equalToConstant: widthConstant))
+        }
+        
+        if heightConstant > 0 {
+            anchors.append(heightAnchor.constraint(equalToConstant: heightConstant))
+        }
+        
+        anchors.forEach({$0.isActive = true})
+        
+        return anchors
+    }
+    
+    
+    
+}
+
+
+extension UIImageView {
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFill) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { () -> Void in
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFill) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
+}
--- DropDown-List/ViewController.swift
+++ DropDown-List/ViewController.swift
@@ -8,18 +8,128 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource, UITextFieldDelegate {
+   
+    
+    //Cell Id for Table View Cell
+    let dropDownId = "cellId"
+    //Array from which TextField data is compared.
+    let array = ["Messi","Cristiano Ronaldo","David Beckham", "Tashi Sidd", "Ronaldinho", "Ronaldo", "Ibrahimnovic", "","Neymar","manuel Neuer","Luis Suarez","Gareth Bale","Andres Iniesta","Lewandoski","Rodriguez","Messi","Cristiano Ronaldo","David Beckham", "Tashi Sidd", "Ronaldinho", "Ronaldo", "Ibrahimnovic", "","Neymar","manuel Neuer","Luis Suarez","Gareth Bale","Andres Iniesta","Lewandoski","Rodriguez","Messi","Cristiano Ronaldo","David Beckham", "Tashi Sidd", "Ronaldinho", "Ronaldo", "Ibrahimnovic", "","Neymar","manuel Neuer","Luis Suarez","Gareth Bale","Andres Iniesta","Lewandoski","Rodriguez","Messi","Cristiano Ronaldo","David Beckham", "Tashi Sidd", "Ronaldinho", "Ronaldo", "Ibrahimnovic", "","Neymar","manuel Neuer","Luis Suarez","Gareth Bale","Andres Iniesta","Lewandoski","Rodriguez","Messi","Cristiano Ronaldo","David Beckham", "Tashi Sidd", "Ronaldinho", "Ronaldo", "Ibrahimnovic", "","Neymar","manuel Neuer","Luis Suarez","Gareth Bale","Andres Iniesta","Lewandoski","Rodriguez"]
+    //Temporary array which is filled when a particular match is attained
+    var autoComplete : [String] = []
+    
+    //Creating TextField Programmatically
+    var textfield : UITextField  = { //Creating a textfield with UITextfield closure
+        //Creating ImageView inside an textfield
+        var image : UIImageView = { //Creating a textfield with UIImageView closure
+           let ig = UIImageView()
+            ig.image = UIImage(named : "drop-down")
+            return ig //Returning ig
+        }()
+        let tf = UITextField()
+        tf.backgroundColor = UIColor(white: 0.7, alpha: 0.8)
+        //Adding the Dropdown Image in the textfield
+        tf.addSubview(image)
+        tf.addConstraintsWithFormat(format: "H:[v0(50)]|", views: image)
+        tf.addConstraintsWithFormat(format: "V:[v0(50)]|", views: image)
+        tf.clearsOnBeginEditing = true
+        tf.clearButtonMode = UITextFieldViewMode.always
+        return tf
+    }()
+    
+    
+    //Creating table view
+    lazy var tableView : UITableView = {
+        let tv = UITableView()
+        tv.bounces = false
+        //Setting delegate and datasource
+        tv.delegate = self
+        tv.dataSource = self
+        return tv
+    }()
+    
+    //Creating TextView
+    var textView : UITextView = {
+       let tv = UITextView()
+        tv.text = "Search for your favourite football player. Example Tashi Sidd"
+        tv.isEditable = false
+        tv.backgroundColor = .clear
+        return tv
+    }()
+    
+    //MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addNewView()
+        textfield.delegate = self
+    }
+    func addNewView() {
+        view.addSubview(textfield)
+        view.addSubview(tableView)
+        view.addSubview(textView)
+        //Registering the cell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: dropDownId)
+        view.addConstraintsWithFormat(format : "V:|-200-[v0(50)]-[v1(200)]",views: textfield,tableView)
+        
+        view.addConstraintsWithFormat(format: "H:|-10-[v0]-10-|", views: textfield)
+        view.addConstraintsWithFormat(format: "H:|-10-[v0]-10-|", views: tableView)
+        view.addConstraintsWithFormat(format: "H:|[v0]|", views: textView)
+        view.addConstraintsWithFormat(format: "V:|-150-[v0(50)]", views: textView)
+        view.hideKeyboardWhenTappedAround()
+        //hide the table view initially
+        tableView.isHidden = true
+        
+    }
+//TableView Functions
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: dropDownId, for: indexPath)
+        if textfield.isEditing{
+        cell.textLabel?.text = autoComplete[indexPath.row]
+        return cell
+        }
+        return cell
+    }
+    //Returning sections as per match
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return autoComplete.count
+    }
+    //After selecting Particular match!
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCell: UITableViewCell = tableView.cellForRow(at: indexPath)!
+        textfield.text = selectedCell.textLabel?.text
+        view.endEditing(true)
+        tableView.isHidden = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//Text Field setups
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textfield.hasText{
+            tableView.reloadData() //Reload Table View
+            tableView.isHidden = false
+        }
+        
+        let substring = (textField.text!.lowercased() as NSString).replacingCharacters(in: range, with: string)
+        seachAutoCompleteWithSubstring(substring)
+        return true
     }
-
-
+    
+    func seachAutoCompleteWithSubstring(_ substring : String){
+        autoComplete.removeAll(keepingCapacity: false)
+        if textfield.isEditing{
+            for key in array{
+                let myString:NSString! = key.lowercased() as NSString
+                
+                let substringRange :NSRange! = myString.range(of: substring)
+                
+                if (substringRange.location  == 0)
+                {
+                    autoComplete.append(key)
+                }
+                tableView.reloadData()
+            }
+        }else {
+            tableView.isHidden = true
+        }
+    }
 }
 
